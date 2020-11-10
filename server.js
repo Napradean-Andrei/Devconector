f1f0d75147af646afa5b490c9fb5ed1e415bfaae
Napradean Andrei <48839967+Napradean-Andrei@users.noreply.github.com>
2020-11-10 23:29:36 +0200

    Updates

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const app = express();
 
 connectDB();
 
+app.use(express.json({ extended: false}));
+
 app.get('/', (req,res) => res.send('API Running') );
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server startedon port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
